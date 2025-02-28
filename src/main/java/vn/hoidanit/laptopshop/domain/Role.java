04042eba48057d63aecf4a0a7166471c28d0c583
ThaiVanLam <thaivanlam373@gmail.com>
2025-02-28 11:44:43 +0700

    video #80

--- src/main/java/vn/hoidanit/laptopshop/domain/Role.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -1,11 +1,17 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "roles")
 public class Role {
 
     @Id
@@ -13,6 +19,10 @@ public class Role {
     private long id;
     private String name;
     private String description;
+
+    // role one to many users
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 
     public long getId() {
         return id;
