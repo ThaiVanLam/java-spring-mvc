41688de0816f35c3571eccd70d3ef26168bfcd5b
ThaiVanLam <thaivanlam373@gmail.com>
2025-03-16 20:22:04 +0700

    video #101

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -2,6 +2,7 @@ package vn.hoidanit.laptopshop.controller.admin;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.Valid;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UploadService;
 import vn.hoidanit.laptopshop.service.UserService;
@@ -11,6 +12,8 @@ import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -87,8 +90,17 @@ public class UserController {
     }
 
     @PostMapping("admin/user/create")
-    public String createUserPage(@ModelAttribute("newUser") User hoidanit,
+    public String createUserPage(@ModelAttribute("newUser") @Valid User hoidanit, BindingResult bindingResult,
             @RequestParam("hoidanitFile") MultipartFile file) {
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(error.getObjectName() + " - " + error.getDefaultMessage());
+        }
+
+        // validate
+
+        //
+
         String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
         String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
         hoidanit.setPassword(hashPassword);
